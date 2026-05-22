b5b59d4443414eab55e4abe2c887fdf0f72b4491
MaiconDouglas-dev <maicon.timot8@gmail.com>
2026-05-22 10:24:41 -0300

    Conexão com banco de dados e adicao do migrations

---
+++ Migrations/20260522132233_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClivoVetApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TB_CLIVO_PET",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    Nome = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Especie = table.Column<string>(type: "NVARCHAR2(50)", maxLength: 50, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    Peso = table.Column<double>(type: "BINARY_DOUBLE", nullable: false),
+                    StatusAtividade = table.Column<string>(type: "NVARCHAR2(20)", maxLength: 20, nullable: false),
+                    UltimaSincronizacao = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TB_CLIVO_PET", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TB_CLIVO_PET");
+        }
+    }
+}
